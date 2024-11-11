2cc93df8cef83d9ede911727faea9941a8a73a8a
RóbertUszkai <robert.uszkai@r34dy.io>
2024-11-11 20:59:45 +0100

    init

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {Login$Params} from '../api/maya/api/mayacomposite-api/fn/login-controller/login';
+import {LoginControllerService} from '../api/maya/api/mayacomposite-api/services/login-controller.service';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private loginService: LoginControllerService, private router: Router) {}
+
+  onSubmit() {
+    const loginParams: Login$Params = {
+      body: { email: this.email, password: this.password }
+    };
+
+    this.loginService.login(loginParams).subscribe(
+      (response: string) => { // Specify that the response is a string
+        console.log('Login successful:', response);
+        localStorage.setItem('token', response); // Store the token as a string
+        this.router.navigate(['/dashboard']);
+      },
+      (error) => {
+        console.error('Login failed:', error);
+        alert('Login failed: Please check your credentials and try again.');
+      }
+    );
+  }
+
 
 }
